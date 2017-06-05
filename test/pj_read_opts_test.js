ed68c57fda579d55b75c4f1abbd589f9b515b470
Matthew Bloch <masiyou@gmail.com>
2017-06-05 16:31:17 -0400

    Update test after nad/esri update

--- test/pj_read_opts_test.js
+++ test/pj_read_opts_test.js
@@ -9,7 +9,7 @@ function expect(initStr, expected) {
 }
 
 describe('pj_open_lib.js pj_read_opts()', function () {
-  expect('esri:2000', '+proj=tmerc +lat_0=0 +lon_0=-62 +k=0.999500 +x_0=400000 +y_0=0 +ellps=clrk80 +units=m +no_defs +no_defs');
+  expect('esri:2000', '+proj=tmerc +lat_0=0 +lon_0=-62 +k=0.999500 +x_0=400000 +y_0=0 +ellps=clrk80 +units=m +no_defs');
   expect('nad27:101', '+proj=tmerc +datum=NAD27 +lon_0=-85d50 +lat_0=30d30 +k=.99996 +x_0=152400.3048006096 +y_0=0 +no_defs');
   expect('epsg:2222', '+proj=tmerc +lat_0=31 +lon_0=-110.1666666666667 +k=0.9999 +x_0=213360 +y_0=0 +datum=NAD83 +units=ft +no_defs');
   expect('IGNF:AMST63', '+title=Ile d\'Amsterdam 1963 +proj=geocent +towgs84=109.7530,-528.1330,-362.2440 +a=6378388.0000 +rf=297.0000000000000 +units=m +no_defs');
