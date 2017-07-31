7ecb6a2bfc965cfabb49a8ca727dd0f512f0e529
Matthew Bloch <masiyou@gmail.com>
2017-07-30 23:06:34 -0400

    add test file for GitHub issues

---
+++ test/github_issues_test.js
@@ -0,0 +1,10 @@
+var assert = require('assert'),
+    api = require('../');
+
+describe('#5 (improper nadgrids error)', function () {
+  it ('+nadgrids=@null should be ignored', function() {
+
+
+  })
+
+});
